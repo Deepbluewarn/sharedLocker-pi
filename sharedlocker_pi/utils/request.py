2b00d4abd67e919302d2e530be37e54ea0bff953
bluewarn <n5lp97@gmail.com>
2024-11-12 22:09:58 +0900

    request 모듈 예외 처리 추가

--- sharedlocker_pi/utils/request.py
+++ sharedlocker_pi/utils/request.py
@@ -1,21 +1,41 @@
 import requests
 
-# GET 요청 보내기
 def GET(url, params=None):
-    response = requests.get(url, params=params)
-    if response.status_code == 200:
-        print("GET 요청 성공")
-        print("응답 데이터:", response.text)
-    else:
-        print("GET 요청 실패")
+    try:
+        response = requests.get(url, params=params)
+        response.raise_for_status()  # HTTPError가 발생하면 예외를 발생시킵니다.
+        
+        if response.status_code == 200:
+            print("GET 요청 성공")
+            print("응답 데이터:", response.text)
+        else:
+            print("GET 요청 실패")
+            print("상태 코드:", response.status_code)
+            print("응답 데이터:", response.text if response.text else "No response text")
+    except requests.exceptions.HTTPError as http_err:
+        print(f"HTTP error occurred: {http_err}")
         print("상태 코드:", response.status_code)
+        print("응답 데이터:", response.text if response.text else "No response text")
+    except Exception as err:
+        print(f"Other error occurred: {err}")
+    return response.text if response.text else None
 
-# POST 요청 보내기
 def POST(url, data=None, json=None, headers=None):
-    response = requests.post(url, data=data, json=json, headers=headers)
-    if response.status_code == 200:
-        print("POST 요청 성공")
-        print("응답 데이터:", response.text)
-    else:
-        print("POST 요청 실패")
+    try:
+        response = requests.post(url, data=data, json=json, headers=headers)
+        response.raise_for_status()  # HTTPError가 발생하면 예외를 발생시킵니다.
+        
+        if response.status_code == 200:
+            print("POST 요청 성공")
+            print("응답 데이터:", response.text)
+        else:
+            print("POST 요청 실패")
+            print("상태 코드:", response.status_code)
+            print("응답 데이터:", response.text if response.text else "No response text")
+    except requests.exceptions.HTTPError as http_err:
+        print(f"HTTP error occurred: {http_err}")
         print("상태 코드:", response.status_code)
+        print("응답 데이터:", response.text if response.text else "No response text")
+    except Exception as err:
+        print(f"Other error occurred: {err}")
+    return response.text if response.text else None
